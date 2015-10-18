720bc60aac068d2079deae36a4ad707cac4b65e5
Shadi Akiki <shadiakiki1986@gmail.com>
2015-10-18 11:48:25 +0400

    updating gh-pages from master

--- js/ServerVersion.js
+++ js/ServerVersion.js
@@ -17,25 +17,32 @@ function ServerVersion($scope,$http) {
   $scope.$on("savedOnServer2",function() { $scope.getVersion(); });
 
   $scope.getVersion = function() {
-        if(!$scope.$parent.config.api.ServerVersion) {
-          console.log("get version not configured");
-          return;
-        }
+    if(!$scope.$parent.config.api.ServerVersion) {
+      console.log("get version not configured");
+      return;
+    }
 
-        $scope.$parent.gvSt="Getting";
-        $http.get($scope.$parent.config.api.ServerVersion).
-          then(function(response) {
-            $scope.versioningRemote.appVersion = response.data.appVersion;
-            $scope.versioningRemote.appDate = response.data.appDate;
-            $scope.versioningRemote.dataVersion = response.data.dataVersion;
-            $scope.versioningRemote.dataDate = response.data.dataDate;
-            $scope.$parent.gvSt="None";
-          }, function(errResponse) {
-            $scope.versioningRemote.appVersion = "N/A";
-            $scope.versioningRemote.appDate = "N/A";
-            $scope.versioningRemote.dataVersion = "N/A";
-            $scope.versioningRemote.dataDate = "N/A";
-            $scope.$parent.gvSt="Error";
-          });
-  };
+    $scope.$parent.gvSt="Getting";
+    $scope.versioningRemote.appVersion = "-";
+    $scope.versioningRemote.appDate = "-";
+    $scope.versioningRemote.dataVersion = "-";
+    $scope.versioningRemote.dataDate = "-";
+
+    $http.get($scope.$parent.config.api.ServerVersion).
+      then(function(response) {
+        $scope.versioningRemote.appVersion = response.data.appVersion;
+        $scope.versioningRemote.appDate = response.data.appDate;
+        $scope.versioningRemote.dataVersion = response.data.dataVersion;
+        $scope.versioningRemote.dataDate = response.data.dataDate;
+        $scope.$parent.gvSt="None";
+      }, function(errResponse) {
+        console.log("http get server version err");
+        $scope.versioningRemote.appVersion = "N/A";
+        $scope.versioningRemote.appDate = "N/A";
+        $scope.versioningRemote.dataVersion = "N/A";
+        $scope.versioningRemote.dataDate = "N/A";
+        $scope.$parent.gvSt="Error";
+      });
+
+  }; // end getVersion
 }
